0d3813ee9f458bd5683ac5094eb4494d24035309
Andrea Gottardo <andrea@gottardo.me>
2019-04-27 13:11:31 -0700

    Initial Commit

---
+++ HackerNewsTV/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HackerNewsTV
+//
+//  Created by Andrea Gottardo on 2019-04-27.
+//  Copyright © 2019 Andrea Gottardo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
